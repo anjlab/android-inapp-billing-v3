1079dbcb863baefcd4d6e069311e5458f89b29ae
Monica Curti <moni890185@gmail.com>
2017-06-30 10:03:01 +0100

    insert support for introductory price for subscriptions.

--- library/src/androidTest/java/com/anjlab/android/iab/v3/SkuDetailsParcelableTest.java
+++ library/src/androidTest/java/com/anjlab/android/iab/v3/SkuDetailsParcelableTest.java
@@ -2,38 +2,61 @@ package com.anjlab.android.iab.v3;
 
 import android.os.Parcel;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-public class SkuDetailsParcelableTest
-{
-    @Test
-    public void testParcelable() throws Exception
-    {
-        String skuDetailsJson =
-                "{\"productId\": \"test-id\",\"type\": \"inapp\",\"price\": \"€7.99\","+
-                "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\","+
-                "\"title\": \"Test Product\",\"description\": \"A great product for testing.\"}";
-        JSONObject details = new JSONObject(skuDetailsJson);
-        SkuDetails skuDetails = new SkuDetails(details);
+public class SkuDetailsParcelableTest {
+  @Test public void testInAppParcelable() throws Exception {
+    String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"inapp\",\"price\": \"€7.99\","
+        + "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\","
+        + "\"title\": \"Test Product\",\"description\": \"A great product for testing.\"}";
+    testParcelable(skuDetailsJson);
+  }
 
-        Parcel parcel = Parcel.obtain();
+  @Test public void testSubsParcelable() throws Exception {
+    String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"subs\",\"price\": \"€7.99\","
+        + "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\","
+        + "\"title\": \"Test Product\",\"description\": \"A great product for testing.\"}";
+    testParcelable(skuDetailsJson);
+  }
 
-        skuDetails.writeToParcel(parcel, 0);
+  @Test public void testSubsWithIntroductoryPriceParcelable() throws Exception {
+    String skuDetailsJson =
+        "{\"productId\":\"test_id\",\"type\":\"subs\",\"price\":\"£45.00\",\"price_amount_micros\""
+            + ":45000000,\"price_currency_code\":\"GBP\",\""
+            + "introductoryPricePeriod\":\"P1Y\",\"introductoryPrice\":"
+            + "\"£22.50\",\"introductoryPriceCycles\":1,\"title\":\"Test title\",\"description\":\"Test description\"}";
+    testParcelable(skuDetailsJson);
+  }
 
-        parcel.setDataPosition(0);
+  private void testParcelable(String skuDetailsJson) throws JSONException {
+    JSONObject details = new JSONObject(skuDetailsJson);
+    SkuDetails skuDetails = new SkuDetails(details);
 
-        SkuDetails result = SkuDetails.CREATOR.createFromParcel(parcel);
+    Parcel parcel = Parcel.obtain();
 
-        assertEquals(skuDetails.productId, result.productId);
-        assertEquals(skuDetails.priceLong, result.priceLong);
-        assertEquals(skuDetails.priceText, result.priceText);
-        assertEquals(skuDetails.priceValue, result.priceValue);
-        assertEquals(skuDetails.description, result.description);
-        assertEquals(skuDetails.isSubscription, result.isSubscription);
-        assertEquals(skuDetails.currency, result.currency);
-        assertEquals(skuDetails.title, result.title);
-    }
+    skuDetails.writeToParcel(parcel, 0);
+
+    parcel.setDataPosition(0);
+
+    SkuDetails result = SkuDetails.CREATOR.createFromParcel(parcel);
+
+    assertEquals(skuDetails.productId, result.productId);
+    assertEquals(skuDetails.priceLong, result.priceLong);
+    assertEquals(skuDetails.priceText, result.priceText);
+    assertEquals(skuDetails.priceValue, result.priceValue);
+    assertEquals(skuDetails.description, result.description);
+    assertEquals(skuDetails.isSubscription, result.isSubscription);
+    assertEquals(skuDetails.currency, result.currency);
+    assertEquals(skuDetails.title, result.title);
+    assertEquals(skuDetails.priceValue, result.priceValue);
+    assertEquals(skuDetails.priceLong, result.priceLong);
+    assertEquals(skuDetails.priceText, result.priceText);
+    assertEquals(skuDetails.introductoryPrice, result.introductoryPrice);
+    assertEquals(skuDetails.introductoryPricePeriod, result.introductoryPricePeriod);
+    assertEquals(skuDetails.introductoryPriceCycles, result.introductoryPriceCycles);
+  }
 }
