e8109990223baa8425489f19fd728fc67f3c9f05
Yauhen Landarski <yauhen.landarski@apalon.com>
2017-06-30 13:21:32 +0300

    Added boolean values for detect subscription types

--- library/src/androidTest/java/com/anjlab/android/iab/v3/util/ResourcesUtil.java
+++ library/src/androidTest/java/com/anjlab/android/iab/v3/util/ResourcesUtil.java
@@ -7,12 +7,23 @@ public class ResourcesUtil
     public static String loadFile(String path)
     {
         String result = "";
-        Scanner sc = new Scanner(ResourcesUtil.class.getClassLoader().getResourceAsStream(path));
-        while (sc.hasNextLine())
+        Scanner sc = null;
+        try
         {
-            result += sc.nextLine();
+            sc = new Scanner(ResourcesUtil.class.getClassLoader().getResourceAsStream(path));
+            while (sc.hasNextLine())
+            {
+                result += sc.nextLine();
+            }
+            sc.close();
         }
-        sc.close();
+        finally
+        {
+            if (sc != null)
+            {
+                sc.close();
+            }
+        }
         return result;
     }
 }
