cc6508718e109bcdb80ed193be33c13005a74da5
Monica Curti <moni890185@gmail.com>
2017-07-01 11:05:51 +0100

    use also same indent.

--- library/src/androidTest/java/com/anjlab/android/iab/v3/SkuDetailsParcelableTest.java
+++ library/src/androidTest/java/com/anjlab/android/iab/v3/SkuDetailsParcelableTest.java
@@ -9,60 +9,60 @@ import static junit.framework.Assert.assertEquals;
 
 public class SkuDetailsParcelableTest
 {
-  @Test
-  public void testInAppParcelable() throws Exception
-  {
-    String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"inapp\",\"price\": \"€7.99\"," +
-            "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\"," + "\"title\": \"Test Product\"," +
-            "\"description\": \"A great product for testing.\"}";
-    testParcelable(skuDetailsJson);
-  }
+    @Test
+    public void testInAppParcelable() throws Exception
+    {
+        String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"inapp\",\"price\": \"€7.99\"," +
+                "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\"," + "\"title\": \"Test Product\"," +
+                "\"description\": \"A great product for testing.\"}";
+        testParcelable(skuDetailsJson);
+    }
 
-  @Test
-  public void testSubsParcelable() throws Exception
-  {
-    String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"subs\",\"price\": \"€7.99\"," +
-            "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\"," + "\"title\": \"Test Product\"," +
-            "\"description\": \"A great product for testing.\"}";
-    testParcelable(skuDetailsJson);
-  }
+    @Test
+    public void testSubsParcelable() throws Exception
+    {
+        String skuDetailsJson = "{\"productId\": \"test-id\",\"type\": \"subs\",\"price\": \"€7.99\"," +
+                "\"price_amount_micros\": \"7990000\",\"price_currency_code\": \"GBP\"," + "\"title\": \"Test Product\"," +
+                "\"description\": \"A great product for testing.\"}";
+        testParcelable(skuDetailsJson);
+    }
 
-  @Test
-  public void testSubsWithIntroductoryPriceParcelable() throws Exception
-  {
-    String skuDetailsJson = "{\"productId\":\"test_id\",\"type\":\"subs\",\"price\":\"£45.00\",\"price_amount_micros\"" +
-            ":45000000," + "\"price_currency_code\":\"GBP\",\"" + "introductoryPricePeriod\":\"P1Y\"," +
-            "\"introductoryPrice\":" + "\"£22.50\",\"introductoryPriceCycles\":1,\"title\":\"Test title\"," +
-            "\"description\":\"Test description\"}";
-    testParcelable(skuDetailsJson);
-  }
+    @Test
+    public void testSubsWithIntroductoryPriceParcelable() throws Exception
+    {
+        String skuDetailsJson = "{\"productId\":\"test_id\",\"type\":\"subs\",\"price\":\"£45.00\",\"price_amount_micros\"" +
+                ":45000000," + "\"price_currency_code\":\"GBP\",\"" + "introductoryPricePeriod\":\"P1Y\"," +
+                "\"introductoryPrice\":" + "\"£22.50\",\"introductoryPriceCycles\":1,\"title\":\"Test title\"," +
+                "\"description\":\"Test description\"}";
+        testParcelable(skuDetailsJson);
+    }
 
-  private void testParcelable(String skuDetailsJson) throws JSONException
-  {
-    JSONObject details = new JSONObject(skuDetailsJson);
-    SkuDetails skuDetails = new SkuDetails(details);
+    private void testParcelable(String skuDetailsJson) throws JSONException
+    {
+        JSONObject details = new JSONObject(skuDetailsJson);
+        SkuDetails skuDetails = new SkuDetails(details);
 
-    Parcel parcel = Parcel.obtain();
+        Parcel parcel = Parcel.obtain();
 
-    skuDetails.writeToParcel(parcel, 0);
+        skuDetails.writeToParcel(parcel, 0);
 
-    parcel.setDataPosition(0);
+        parcel.setDataPosition(0);
 
-    SkuDetails result = SkuDetails.CREATOR.createFromParcel(parcel);
+        SkuDetails result = SkuDetails.CREATOR.createFromParcel(parcel);
 
-    assertEquals(skuDetails.productId, result.productId);
-    assertEquals(skuDetails.priceLong, result.priceLong);
-    assertEquals(skuDetails.priceText, result.priceText);
-    assertEquals(skuDetails.priceValue, result.priceValue);
-    assertEquals(skuDetails.description, result.description);
-    assertEquals(skuDetails.isSubscription, result.isSubscription);
-    assertEquals(skuDetails.currency, result.currency);
-    assertEquals(skuDetails.title, result.title);
-    assertEquals(skuDetails.priceValue, result.priceValue);
-    assertEquals(skuDetails.priceLong, result.priceLong);
-    assertEquals(skuDetails.priceText, result.priceText);
-    assertEquals(skuDetails.introductoryPrice, result.introductoryPrice);
-    assertEquals(skuDetails.introductoryPricePeriod, result.introductoryPricePeriod);
-    assertEquals(skuDetails.introductoryPriceCycles, result.introductoryPriceCycles);
-  }
+        assertEquals(skuDetails.productId, result.productId);
+        assertEquals(skuDetails.priceLong, result.priceLong);
+        assertEquals(skuDetails.priceText, result.priceText);
+        assertEquals(skuDetails.priceValue, result.priceValue);
+        assertEquals(skuDetails.description, result.description);
+        assertEquals(skuDetails.isSubscription, result.isSubscription);
+        assertEquals(skuDetails.currency, result.currency);
+        assertEquals(skuDetails.title, result.title);
+        assertEquals(skuDetails.priceValue, result.priceValue);
+        assertEquals(skuDetails.priceLong, result.priceLong);
+        assertEquals(skuDetails.priceText, result.priceText);
+        assertEquals(skuDetails.introductoryPrice, result.introductoryPrice);
+        assertEquals(skuDetails.introductoryPricePeriod, result.introductoryPricePeriod);
+        assertEquals(skuDetails.introductoryPriceCycles, result.introductoryPriceCycles);
+    }
 }
--- library/src/main/java/com/anjlab/android/iab/v3/SkuDetails.java
+++ library/src/main/java/com/anjlab/android/iab/v3/SkuDetails.java
@@ -25,144 +25,144 @@ import java.util.Locale;
 public class SkuDetails implements Parcelable
 {
 
-  public final String productId;
+    public final String productId;
 
-  public final String title;
+    public final String title;
 
-  public final String description;
+    public final String description;
 
-  public final boolean isSubscription;
+    public final boolean isSubscription;
 
-  public final String currency;
+    public final String currency;
 
-  public final Double priceValue;
+    public final Double priceValue;
 
-  /**
-   * Use this value to return the raw price from the product.
-   * This allows math to be performed without needing to worry about errors
-   * caused by floating point representations of the product's price.
-   * <p>
-   * This is in micros from the Play Store.
-   */
-  public final long priceLong;
+    /**
+     * Use this value to return the raw price from the product.
+     * This allows math to be performed without needing to worry about errors
+     * caused by floating point representations of the product's price.
+     * <p>
+     * This is in micros from the Play Store.
+     */
+    public final long priceLong;
 
-  public final String priceText;
-  /**
-   * Introductory price of a subscription fields
-   * Returned only for subscriptions which have an introductory period configured
-   * ref. https://developer.android.com/google/play/billing/billing_reference.html#getSkuDetails
-   */
-  public final String introductoryPrice;
+    public final String priceText;
+    /**
+     * Introductory price of a subscription fields
+     * Returned only for subscriptions which have an introductory period configured
+     * ref. https://developer.android.com/google/play/billing/billing_reference.html#getSkuDetails
+     */
+    public final String introductoryPrice;
 
-  public final String introductoryPricePeriod;
+    public final String introductoryPricePeriod;
 
-  public final Double introductoryPriceCycles;
+    public final Double introductoryPriceCycles;
 
-  public SkuDetails(JSONObject source) throws JSONException
-  {
-    String responseType = source.optString(Constants.RESPONSE_TYPE);
-    if (responseType == null)
+    public SkuDetails(JSONObject source) throws JSONException
     {
-      responseType = Constants.PRODUCT_TYPE_MANAGED;
-    }
-    productId = source.optString(Constants.RESPONSE_PRODUCT_ID);
-    title = source.optString(Constants.RESPONSE_TITLE);
-    description = source.optString(Constants.RESPONSE_DESCRIPTION);
-    isSubscription = responseType.equalsIgnoreCase(Constants.PRODUCT_TYPE_SUBSCRIPTION);
-    currency = source.optString(Constants.RESPONSE_PRICE_CURRENCY);
-    priceLong = source.optLong(Constants.RESPONSE_PRICE_MICROS);
-    priceValue = priceLong / 1000000d;
-    priceText = source.optString(Constants.RESPONSE_PRICE);
-    introductoryPrice = source.optString(Constants.RESPONSE_INTRODUCTORY_PRICE);
-    introductoryPricePeriod = source.optString(Constants.RESPONSE_INTRODUCTORY_PRICE_PERIOD);
-    introductoryPriceCycles = source.optDouble(Constants.RESPONSE_INTRODUCTORY_PRICE_CYCLES);
-  }
-
-  @Override
-  public String toString()
-  {
-    return String.format(Locale.US, "%s: %s(%s) %f in %s (%s)", productId, title, description,
-            priceValue, currency, priceText);
-  }
-
-  @Override
-  public boolean equals(Object o)
-  {
-    if (this == o)
-    {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass())
-    {
-      return false;
-    }
-
-    SkuDetails that = (SkuDetails) o;
-
-    if (isSubscription != that.isSubscription)
-    {
-      return false;
-    }
-    return !(productId != null ? !productId.equals(that.productId) : that.productId != null);
-  }
-
-  @Override
-  public int hashCode()
-  {
-    int result = productId != null ? productId.hashCode() : 0;
-    result = 31 * result + (isSubscription ? 1 : 0);
-    return result;
-  }
-
-  @Override
-  public int describeContents()
-  {
-    return 0;
-  }
-
-  @Override
-  public void writeToParcel(Parcel dest, int flags)
-  {
-    dest.writeString(this.productId);
-    dest.writeString(this.title);
-    dest.writeString(this.description);
-    dest.writeByte(this.isSubscription ? (byte) 1 : (byte) 0);
-    dest.writeString(this.currency);
-    dest.writeDouble(this.priceValue);
-    dest.writeLong(this.priceLong);
-    dest.writeString(this.priceText);
-    dest.writeString(this.introductoryPrice);
-    dest.writeString(this.introductoryPricePeriod);
-    dest.writeDouble(this.introductoryPriceCycles);
-  }
-
-  protected SkuDetails(Parcel in)
-  {
-    this.productId = in.readString();
-    this.title = in.readString();
-    this.description = in.readString();
-    this.isSubscription = in.readByte() != 0;
-    this.currency = in.readString();
-    this.priceValue = in.readDouble();
-    this.priceLong = in.readLong();
-    this.priceText = in.readString();
-    this.introductoryPrice = in.readString();
-    this.introductoryPricePeriod = in.readString();
-    this.introductoryPriceCycles = in.readDouble();
-  }
-
-  public static final Creator<SkuDetails> CREATOR = new Creator<SkuDetails>()
-  {
-    @Override
-    public SkuDetails createFromParcel(Parcel source)
-    {
-      return new SkuDetails(source);
+        String responseType = source.optString(Constants.RESPONSE_TYPE);
+        if (responseType == null)
+        {
+            responseType = Constants.PRODUCT_TYPE_MANAGED;
+        }
+        productId = source.optString(Constants.RESPONSE_PRODUCT_ID);
+        title = source.optString(Constants.RESPONSE_TITLE);
+        description = source.optString(Constants.RESPONSE_DESCRIPTION);
+        isSubscription = responseType.equalsIgnoreCase(Constants.PRODUCT_TYPE_SUBSCRIPTION);
+        currency = source.optString(Constants.RESPONSE_PRICE_CURRENCY);
+        priceLong = source.optLong(Constants.RESPONSE_PRICE_MICROS);
+        priceValue = priceLong / 1000000d;
+        priceText = source.optString(Constants.RESPONSE_PRICE);
+        introductoryPrice = source.optString(Constants.RESPONSE_INTRODUCTORY_PRICE);
+        introductoryPricePeriod = source.optString(Constants.RESPONSE_INTRODUCTORY_PRICE_PERIOD);
+        introductoryPriceCycles = source.optDouble(Constants.RESPONSE_INTRODUCTORY_PRICE_CYCLES);
     }
 
     @Override
-    public SkuDetails[] newArray(int size)
+    public String toString()
     {
-      return new SkuDetails[size];
+        return String.format(Locale.US, "%s: %s(%s) %f in %s (%s)", productId, title, description,
+                priceValue, currency, priceText);
     }
-  };
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        SkuDetails that = (SkuDetails) o;
+
+        if (isSubscription != that.isSubscription)
+        {
+            return false;
+        }
+        return !(productId != null ? !productId.equals(that.productId) : that.productId != null);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = productId != null ? productId.hashCode() : 0;
+        result = 31 * result + (isSubscription ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public int describeContents()
+    {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags)
+    {
+        dest.writeString(this.productId);
+        dest.writeString(this.title);
+        dest.writeString(this.description);
+        dest.writeByte(this.isSubscription ? (byte) 1 : (byte) 0);
+        dest.writeString(this.currency);
+        dest.writeDouble(this.priceValue);
+        dest.writeLong(this.priceLong);
+        dest.writeString(this.priceText);
+        dest.writeString(this.introductoryPrice);
+        dest.writeString(this.introductoryPricePeriod);
+        dest.writeDouble(this.introductoryPriceCycles);
+    }
+
+    protected SkuDetails(Parcel in)
+    {
+        this.productId = in.readString();
+        this.title = in.readString();
+        this.description = in.readString();
+        this.isSubscription = in.readByte() != 0;
+        this.currency = in.readString();
+        this.priceValue = in.readDouble();
+        this.priceLong = in.readLong();
+        this.priceText = in.readString();
+        this.introductoryPrice = in.readString();
+        this.introductoryPricePeriod = in.readString();
+        this.introductoryPriceCycles = in.readDouble();
+    }
+
+    public static final Creator<SkuDetails> CREATOR = new Creator<SkuDetails>()
+    {
+        @Override
+        public SkuDetails createFromParcel(Parcel source)
+        {
+            return new SkuDetails(source);
+        }
+
+        @Override
+        public SkuDetails[] newArray(int size)
+        {
+            return new SkuDetails[size];
+        }
+    };
 }
