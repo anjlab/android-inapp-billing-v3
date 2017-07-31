5a1e03bde51470a16aac4ebb6070432a0ecca3a9
Allan Wang <me@allanwang.ca>
2017-07-31 12:48:53 -0700

    Remove extra import

--- library/src/main/java/com/anjlab/android/iab/v3/BillingProcessor.java
+++ library/src/main/java/com/anjlab/android/iab/v3/BillingProcessor.java
@@ -37,7 +37,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
