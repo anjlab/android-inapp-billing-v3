6e1f1fed50fa45908d982e8b298d7cc1db002a3e
v.markus <v.markus@tinkoff.ru>
2018-05-02 00:14:00 +0300

    Add test for Fragment

--- sample/src/com/anjlab/android/iab/v3/sample2/BillingFragment.java
+++ sample/src/com/anjlab/android/iab/v3/sample2/BillingFragment.java
@@ -1,27 +1,14 @@
-/**
- * Copyright 2014 AnjLab
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.anjlab.android.iab.v3.sample2;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,10 +16,14 @@ import com.anjlab.android.iab.v3.BillingProcessor;
 import com.anjlab.android.iab.v3.SkuDetails;
 import com.anjlab.android.iab.v3.TransactionDetails;
 
-public class MainActivity extends Activity {
-	// SAMPLE APP CONSTANTS
-	private static final String ACTIVITY_NUMBER = "activity_num";
-	private static final String LOG_TAG = "iabv3";
+/**
+ * @author Vitaliy Markus
+ */
+
+public class BillingFragment extends Fragment implements View.OnClickListener {
+    // SAMPLE APP CONSTANTS
+    private static final String ACTIVITY_NUMBER = "activity_num";
+    private static final String LOG_TAG = "iabv3";
 
     // PRODUCT & SUBSCRIPTION IDS
     private static final String PRODUCT_ID = "com.anjlab.test.iab.s2.p5";
@@ -40,60 +31,74 @@ public class MainActivity extends Activity {
     private static final String LICENSE_KEY = null; // PUT YOUR MERCHANT KEY HERE;
     // put your Google merchant id here (as stated in public profile of your Payments Merchant Center)
     // if filled library will provide protection against Freedom alike Play Market simulators
-    private static final String MERCHANT_ID=null;
+    private static final String MERCHANT_ID = null;
 
-	private BillingProcessor bp;
-	private boolean readyToPurchase = false;
+    private BillingProcessor bp;
+    private boolean readyToPurchase = false;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_main, container, false);
 
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView title = root.findViewById(R.id.titleTextView);
+        title.setText(R.string.fragment_title);
 
-		TextView title = (TextView)findViewById(R.id.titleTextView);
-		title.setText(String.format(getString(R.string.title), getIntent().getIntExtra(ACTIVITY_NUMBER, 1)));
-
-        if(!BillingProcessor.isIabServiceAvailable(this)) {
+        if (!BillingProcessor.isIabServiceAvailable(getActivity())) {
             showToast("In-app billing service is unavailable, please upgrade Android Market/Play to version >= 3.9.16");
         }
 
-        bp = new BillingProcessor(this, LICENSE_KEY, MERCHANT_ID, new BillingProcessor.IBillingHandler() {
+        bp = new BillingProcessor(getActivity(), LICENSE_KEY, MERCHANT_ID, new BillingProcessor.IBillingHandler() {
             @Override
             public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
-				showToast("onProductPurchased: " + productId);
+                showToast("onProductPurchased: " + productId);
                 updateTextViews();
             }
+
             @Override
             public void onBillingError(int errorCode, @Nullable Throwable error) {
                 showToast("onBillingError: " + Integer.toString(errorCode));
             }
+
             @Override
             public void onBillingInitialized() {
-				showToast("onBillingInitialized");
+                showToast("onBillingInitialized");
                 readyToPurchase = true;
                 updateTextViews();
             }
+
             @Override
             public void onPurchaseHistoryRestored() {
                 showToast("onPurchaseHistoryRestored");
-                for(String sku : bp.listOwnedProducts())
+                for (String sku : bp.listOwnedProducts())
                     Log.d(LOG_TAG, "Owned Managed Product: " + sku);
-                for(String sku : bp.listOwnedSubscriptions())
+                for (String sku : bp.listOwnedSubscriptions())
                     Log.d(LOG_TAG, "Owned Subscription: " + sku);
                 updateTextViews();
             }
         });
+
+        root.findViewById(R.id.launchMoreButton).setVisibility(View.GONE);
+        root.findViewById(R.id.showInFragment).setVisibility(View.GONE);
+
+        root.findViewById(R.id.purchaseButton).setOnClickListener(this);
+        root.findViewById(R.id.consumeButton).setOnClickListener(this);
+        root.findViewById(R.id.productDetailsButton).setOnClickListener(this);
+        root.findViewById(R.id.subscribeButton).setOnClickListener(this);
+        root.findViewById(R.id.updateSubscriptionsButton).setOnClickListener(this);
+        root.findViewById(R.id.subsDetailsButton).setOnClickListener(this);
+
+        return root;
     }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
+    @Override
+    public void onResume() {
+        super.onResume();
 
-		updateTextViews();
-	}
+        updateTextViews();
+    }
 
-	@Override
+    @Override
     public void onDestroy() {
         if (bp != null)
             bp.release();
@@ -101,20 +106,20 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (!bp.handleActivityResult(requestCode, resultCode, data))
             super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void updateTextViews() {
-        TextView text = (TextView)findViewById(R.id.productIdTextView);
+        TextView text = (TextView) getView().findViewById(R.id.productIdTextView);
         text.setText(String.format("%s is%s purchased", PRODUCT_ID, bp.isPurchased(PRODUCT_ID) ? "" : " not"));
-        text = (TextView)findViewById(R.id.subscriptionIdTextView);
+        text = (TextView) getView().findViewById(R.id.subscriptionIdTextView);
         text.setText(String.format("%s is%s subscribed", SUBSCRIPTION_ID, bp.isSubscribed(SUBSCRIPTION_ID) ? "" : " not"));
     }
 
     private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 
     public void onClick(View v) {
@@ -124,7 +129,7 @@ public class MainActivity extends Activity {
         }
         switch (v.getId()) {
             case R.id.purchaseButton:
-                bp.purchase(this,PRODUCT_ID);
+                bp.purchase(this, PRODUCT_ID);
                 break;
             case R.id.consumeButton:
                 Boolean consumed = bp.consumePurchase(PRODUCT_ID);
@@ -133,11 +138,11 @@ public class MainActivity extends Activity {
                     showToast("Successfully consumed");
                 break;
             case R.id.productDetailsButton:
-				SkuDetails sku = bp.getPurchaseListingDetails(PRODUCT_ID);
+                SkuDetails sku = bp.getPurchaseListingDetails(PRODUCT_ID);
                 showToast(sku != null ? sku.toString() : "Failed to load SKU details");
                 break;
             case R.id.subscribeButton:
-                bp.subscribe(this,SUBSCRIPTION_ID);
+                bp.subscribe(this, SUBSCRIPTION_ID);
                 break;
             case R.id.updateSubscriptionsButton:
                 if (bp.loadOwnedPurchasesFromGoogle()) {
@@ -146,14 +151,8 @@ public class MainActivity extends Activity {
                 }
                 break;
             case R.id.subsDetailsButton:
-				SkuDetails subs = bp.getSubscriptionListingDetails(SUBSCRIPTION_ID);
-				showToast(subs != null ? subs.toString() : "Failed to load subscription details");
-				break;
-			case R.id.launchMoreButton:
-				startActivity(new Intent(this, MainActivity.class).putExtra(ACTIVITY_NUMBER, getIntent().getIntExtra(ACTIVITY_NUMBER, 1) + 1));
-				break;
-            case R.id.showInFragment:
-                startActivity(new Intent(this, BillingFragmentActivity.class));
+                SkuDetails subs = bp.getSubscriptionListingDetails(SUBSCRIPTION_ID);
+                showToast(subs != null ? subs.toString() : "Failed to load subscription details");
                 break;
             default:
                 break;
