89de0cb863422055009adcfcc6a7281efc1a4fe3
Viacheslav <syndarin@users.noreply.github.com>
2019-07-23 14:00:15 +0300

    Support for GetPurchaseHistory (#414)
    * New API for BillingProcessor:
    isRequestBillingHistorySupported - to check if request purchase history supported
    getPurchaseHistory - to request purchase history
    
    * Updated code according to checkstyle
    
    * Updated README to cover new API with getPurchaseHistory.

---
+++ library/src/main/java/com/anjlab/android/iab/v3/BillingCommunicationException.java
@@ -0,0 +1,15 @@
+package com.anjlab.android.iab.v3;
+
+public class BillingCommunicationException extends Exception
+{
+
+    public BillingCommunicationException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public BillingCommunicationException(String message)
+    {
+        super(message);
+    }
+}
