062e23de891eaaaeb54a713cdabefc4a7c58e410
Sergey Glukhov <sergey.glukhov@gmail.com>
2023-12-05 20:16:40 +0300

    update build tooling

--- library/src/main/java/com/anjlab/android/iab/v3/PurchaseInfo.java
+++ library/src/main/java/com/anjlab/android/iab/v3/PurchaseInfo.java
@@ -30,8 +30,7 @@ import android.util.Log;
  * server. An example implementation of how to verify
  * a purchase you can find here:
  * <pre>
- * See <a href="https://github.com/mgoldsborough/google-play-in-app-billing-verification/blob/
- * master/library/GooglePlay/InAppBilling/GooglePlayResponseValidator.php#L64"> here </a>
+ * See <a href="https://t.ly/Hd76A"> here </a>
  * </pre>
  */
 public class PurchaseInfo implements Parcelable
